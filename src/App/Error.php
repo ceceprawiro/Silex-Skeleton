10f5eeceb511953057b183503f737cd75fcc6fe1
Cecep Prawiro <cecep.prawiro@gmail.com>
2016-08-21 18:12:50 +0700

    Update Error.php

--- src/App/Error.php
+++ src/App/Error.php
@@ -15,6 +15,21 @@ class Error
         } else {
             ExceptionHandler::register(false);
         }
+        
+        $app->error(function (\Exception $e, Request $request, $code) use ($app) {
+            if ($app['debug']) {
+                return;
+            }
+
+            $templates = array(
+                'error/'.$code.'.twig',
+                'error/'.substr($code, 0, 2).'x.twig',
+                'error/'.substr($code, 0, 1).'xx.twig',
+                'error/error.twig',
+            );
+
+            return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+        });
 
         return $app;
     }
